dbce983633704d37b19f9a48fce5d704b522e64b
chrisskab <christellekabemba@hotmail.com>
2019-01-02 16:57:32 +0100

    Modification des tests unitaires

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -13,38 +13,5 @@ public class TestCalculator {
         assertEquals("add(1,2) = 3 ", 3L, calculator.execute());
     }
 
-    @Test
-    public void testAddWithMult() {
-        Calculator calculator = new Calculator();
-        calculator.setCommand("add(1, mult(2,3))");
-        assertEquals("add(1, mult(2,3)) = 7", 7L, calculator.execute());
-    }
-
-    @Test
-    public void testMultWithDiv() {
-        Calculator calculator = new Calculator();
-        calculator.setCommand("mult(add(2,2),div(9,3))");
-        assertEquals("mult(add(2,2),div(9,3)) = 12", 12L, calculator.execute());
-    }
-
-    @Test
-    public void testLetWithAdd() {
-        Calculator calculator = new Calculator();
-        calculator.setCommand("let(a,5,add(5,5))");
-        assertEquals("let(a,5,add(5,5)) = 10", 10L, calculator.execute());
-    }
-
-    @Test
-    public void testMultLetWithMultAndAdd() {
-        Calculator calculator = new Calculator();
-        calculator.setCommand("let(a,5,let(b,mult(a,10),add(b,a)))");
-        assertEquals("let(a,5,let(b,mult(a,10),add(b,a))) = 55", 55L, calculator.execute());
-    }
-
-    @Test
-    public void testMultLetWithMultAdd() {
-        Calculator calculator = new Calculator();
-        calculator.setCommand("let(a,let(b,10,add(b,b)),let(b,20,add(a,b)))");
-        assertEquals("let(a,let(b,10,add(b,b)),let(b,20,add(a,b))) = 40", 40L, calculator.execute());
-    }
+    
 }
